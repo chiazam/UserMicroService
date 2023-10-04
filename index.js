f788c067c4dbb78b165452c188897c4d198c20e6
chiazam <54377599+chiazam@users.noreply.github.com>
2023-10-04 01:00:06 +0100

    Validate user done

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const create = require('./paths/create');
+
 const app = express();
 const port = 3000;
 
@@ -10,17 +12,28 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// create user route starts .......
+
 app.post('/create', (req, res) => {
 
     const query = req.query;
 
     const body = req.body;
 
-    // Output the book to the console for debugging
     console.log(body, query);
+
+    let valid = create.validateinfo(body);
+
+    if (valid !== true) {
+
+        res.json(valid);
+
+    }
 
     res.json([body, query]);
 
 });
+
+// create user route ends .......
 
 app.listen(port, () => { console.log(`app listening on port ${port}!`) });
--- paths/create.js
+++ paths/create.js
@@ -1,8 +1,56 @@
 let create = {
 
+    regFullName: /^[a-zA-Z]+ [a-zA-Z]+$/,
+
+    regEmail: /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/,
+
+    regPassword: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+
+    userTypes: ["seller", "buyer"],
+
     validateinfo: (info) => {
 
+        let valid = true;
 
+        let message = "";
+
+        if (!info.hasOwnProperty('fullname') || !info.hasOwnProperty('email') || !info.hasOwnProperty('password') || !info.hasOwnProperty('type')) {
+
+            valid = false;
+
+            message = "Permission Denied";
+
+        } else if (!this.regFullName.test(info.fullname)) {
+
+            valid = false;
+
+            message = "Invalid Full Name";
+
+        } else if (!this.regEmail.test(info.email)) {
+
+            valid = false;
+
+            message = "Invalid Email";
+
+        } else if (!this.regPassword.test(info.password)) {
+
+            valid = false;
+
+            message = "Invalid Password (password must be at least eight characters that contain at least one lowercase letter, one uppercase letter, and one digit)"
+        } else if (!this.userTypes.includes(info.type)) {
+
+            valid = false;
+
+            message = "Invalid Type (type must be either 'seller' or 'buyer'";
+
+        }
+
+        return (valid) ? (valid) : ({
+
+            status: valid,
+            message: message
+
+        });
 
     }
 
