e2d6440840ceb3182697bd787d290f0cc9b595ba
chiazam <54377599+chiazam@users.noreply.github.com>
2023-11-18 15:07:43 +0100

    update Db... Start KYC

--- usermicroservice.sql
+++ usermicroservice.sql
@@ -1,20 +1,23 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               5.7.33 - MySQL Community Server (GPL)
+-- Server version:               8.0.30 - MySQL Community Server - GPL
 -- Server OS:                    Win64
--- HeidiSQL Version:             11.2.0.6213
+-- HeidiSQL Version:             12.1.0.6537
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 -- Dumping structure for table usermicroservice.kyc
 CREATE TABLE IF NOT EXISTS `kyc` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
+  `userid` longtext NOT NULL,
   `address` longtext NOT NULL,
   `phone` longtext NOT NULL,
   `verifyNo` longtext NOT NULL,
@@ -27,12 +30,10 @@ CREATE TABLE IF NOT EXISTS `kyc` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table usermicroservice.kyc: ~0 rows (approximately)
-/*!40000 ALTER TABLE `kyc` DISABLE KEYS */;
-/*!40000 ALTER TABLE `kyc` ENABLE KEYS */;
 
 -- Dumping structure for table usermicroservice.users
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `fullname` longtext NOT NULL,
   `email` longtext NOT NULL,
   `password` longtext NOT NULL,
@@ -40,27 +41,27 @@ CREATE TABLE IF NOT EXISTS `users` (
   `logid` longtext,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
--- Dumping data for table usermicroservice.users: ~0 rows (approximately)
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+-- Dumping data for table usermicroservice.users: ~2 rows (approximately)
+INSERT INTO `users` (`id`, `fullname`, `email`, `password`, `type`, `logid`, `date`) VALUES
+	(1, 'chiazam udekwe', 'azam@gmail.com', '$2b$10$J8LnzrSn3bJ104yY7YYx3OI1ueBcUCkhAIkB1rz/odxSouox0uedi', 'seller', 'NULL', '2023-10-08 03:57:18'),
+	(2, 'elomi udex', 'chielo@gmail.com', '$2b$10$0s7PkoBzcqQqQMIiHN6mlO6IQAm.Qldu4XrnMUSFXTXBrIQFeDhkC', 'buyer', 'WYiku4DobL', '2023-10-08 06:34:51');
 
 -- Dumping structure for table usermicroservice.verify
 CREATE TABLE IF NOT EXISTS `verify` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `token` longtext NOT NULL,
   `userid` longtext NOT NULL,
   `why` longtext NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
--- Dumping data for table usermicroservice.verify: ~0 rows (approximately)
-/*!40000 ALTER TABLE `verify` DISABLE KEYS */;
+-- Dumping data for table usermicroservice.verify: ~1 rows (approximately)
 INSERT INTO `verify` (`id`, `token`, `userid`, `why`) VALUES
-	(1, 'd2ljpd8hl', '1', 'user');
-/*!40000 ALTER TABLE `verify` ENABLE KEYS */;
+	(2, 'epod9TWxI5', '2', 'user');
 
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
